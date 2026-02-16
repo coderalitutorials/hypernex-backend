99a1f0e37ef3da5662ca1992106295a72a4ba4c7
ALi Raza <154666104+coderalitutorials@users.noreply.github.com>
2026-02-15 23:37:33 -0800

    Comment out server setup code in server.js
    Comment out app import and server listening code.

--- server.js
+++ server.js
@@ -2,10 +2,10 @@
 
 
 
-import app from "./app.js";
+// import app from "./app.js";
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`🚀 Server running on port ${PORT}`);
+// });
